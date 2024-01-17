fcc14e7c1e054a3f2375439bbd44c3bcbe3d0849
Gabriel Vieira <gabrielvieira840@gmail.com>
2024-01-17 02:57:46 -0300

    Adicionando CORS, com falha

--- core/routes/api.go
+++ core/routes/api.go
@@ -1,15 +1,26 @@
 package routes
 
 import (
+	"time"
+
 	addresscontroller "github.com/VieiraGabrielAlexandre/luztecnologia-cms-backend/core/controllers/address_controller"
 	clientscontroller "github.com/VieiraGabrielAlexandre/luztecnologia-cms-backend/core/controllers/clients_controller"
 	valuescontroller "github.com/VieiraGabrielAlexandre/luztecnologia-cms-backend/core/controllers/values_controller"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func HandleRequests() {
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
 
+	r.Use(cors.New(config))
 	base := r.Group("/api/")
 	{
 		clientsGroup := base.Group("clients")
@@ -20,8 +31,7 @@ func HandleRequests() {
 		}
 		base.POST("calculate", valuescontroller.Calculate)
 		base.GET("address/viacep/:postalcode", addresscontroller.GetViaCepPostalCode)
-
 	}
 
-	r.Run(":8888")
+	r.Run(":3001")
 }
--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/VieiraGabrielAlexandre/luztecnologia-cms-backend/config"
 	"github.com/VieiraGabrielAlexandre/luztecnologia-cms-backend/core/routes"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+
+	fmt.Printf("-----------\n")
+	fmt.Printf("Loading Routes\n")
+	wg.Add(1)
+	go carregarRotas()
+	fmt.Printf("-----------\n")
+
 	fmt.Printf("-----------\n")
 	fmt.Printf("Loading ENV VARS\n")
 	config.SetEnv()
@@ -17,9 +27,11 @@ func main() {
 	fmt.Printf("Loading DATABASE\n")
 	config.ConnectDatabase()
 	fmt.Printf("-----------\n")
-
-	fmt.Printf("-----------\n")
-	fmt.Printf("Loading Routes\n")
+	wg.Wait()
+}
+func carregarRotas() {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	routes.HandleRequests()
-	fmt.Printf("-----------\n")
+	wg.Done()
 }
